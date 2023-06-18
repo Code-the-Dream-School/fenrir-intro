804fb27cf6fa34d2eaa68f2261eb1f8c3d1c9379
Chelin-Park <134220553+Chelin-Park@users.noreply.github.com>
2023-06-18 14:41:33 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,3 +7,10 @@ let copyright = document.createElement("p");
 copyright.innerHTML = "Chelin Park"+" "+thisYear;
 
 footer.appendChild(copyright);
+
+let messageForm = document.querySelector("leave_message");
+Element.addEventListener("click",functio()
+{
+    let userName = event.target.userName;
+    
+});
