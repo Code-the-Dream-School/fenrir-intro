cd778d1817c299de574be09f874fb1c1ef9b3f09
Chelin-Park <pchin0527@gmail.com>
2023-06-23 22:03:04 -0400

    final rev w/o option question_will do tomorrow

--- js/index.js
+++ js/index.js
@@ -23,30 +23,33 @@ messageForm.addEventListener('submit', function(event) {    //messageForm의 변
     const messageList = messageSection.querySelector('ul'); //ul 쿼리를 참조하는 messageList라는 변수 생성
     //console.log(messageSection);
     
-    //newMessage는 "li"라는 element를 만들고 (리스트 속성), 그 element안에 '<a href='mailto:${email}'>${name}</a> <span> wrote: ${message}</span>과 remove 버튼을 만든다.
+    //newMessage는 "li"라는 element를 만들고 (리스트 속성), 그 element안에 '<a href='mailto:${email}'>${name}</a> <span> wrote: ${message}</span>을 만든다.
     let newMessage = document.createElement("li");
-    newMessage.innerHTML = `<a href='mailto:${email}'>${name}</a> <span> wrote: ${message}</span> <button class = "remove" id = "removal" type = "button"> remove </button>`;    //remove란 버튼 생성
-    messageList.appendChild(newMessage); //messageList변수에 newMessage라는 변수의 값을 '입양'시킨다. 여기서는 ul섹터 안에 들어가게 되는 것.
+    newMessage.innerHTML = `<a href='mailto:${email}'>${name}</a> <span> wrote: ${message}</span> `;    //remove란 버튼 생성 <button class = "remove" id = "removal" type = "button"> remove </button>
 
+    //removeButton이란 변수에 'button'이란 element를 만든다.
+    // Create a new <button> element
+    const removeButton = document.createElement('button');
 
+    // Set the inner text and type attribute of removeButton
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+   
+        removeButton.addEventListener('click',function(event) {    //removeButton에 addEventListener를 통해서 'click'했을 때의 inner function을 선언한다.
+            //entry 라는 변수를 선언해서 removeButton의 parentNode를 찾는다 (이 경우에는 messageList).
+            const entry = removeButton.parentNode;
+            //entry변수를 remove 해서 newMessage의 값을 지운다.
+            entry.remove();
+            //alert("asdf");
+            //newMessage.innerHTML.remove();
+        });
 
-    //here is where my question is...//
+    //messageList element에 newMessage와 remove Button의 값을넣어준다.
+    messageList.appendChild(newMessage) + newMessage.appendChild(removeButton) + newMessage.appendChild(editBu);//messageList변수에 newMessage라는 변수의 값을 '입양'시킨다. 여기서는 ul섹터 안에 들어가게 되는 것. 
 
+    // Show the #messages section including its header if there are messages
+    messageSection.style.display = 'block';
 
-    //removeButton이란 변수에 button selection을 넣는다.
-    let removeButton = document.getElementById("removal");      //I think I did in right way that DOM selection by id "removal" which I assigned to remove button?//
-
-    removeButton.addEventListener("remove",function(event) {    //And I was declared alarm pops up for testing "asdf"//
-        //messageList변수에 removeButton라는 변수의 값을 '입양'시킨다. 여기서는 ul섹터 안에 들어가게 되는 것. + 값을 지운 것을 반영해야 한다.
-        alert("asdf");
-        //newMessage.innerHTML.remove();
-    });
-    
-
-    
-
-
-    
     //입력시 텍스트박스 초기화
     messageForm.reset();
 });
