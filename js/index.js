37bbf4526e2125032a9d1f19e9193d142b60b202
Aleksandra Folkes <adams41191@yahoo.com>
2023-07-26 21:04:19 -0400

    Add index1.css file.

--- js/index.js
+++ js/index.js
@@ -1,52 +1,50 @@
-const today = new Date()
-const thisYear = today.getFullYear()
-const footer = document.querySelector("footer")
-const copyright = document.createElement("p")
+const today = new Date();
+const thisYear = today.getFullYear();
+const footer = document.querySelector("footer");
+const copyright = document.createElement("p");
 
-copyright.innerHTML = `Aleksandra Folkes &#0169 ${thisYear}`
-footer.appendChild(copyright)
+copyright.innerHTML = `Aleksandra Folkes &#0169 ${thisYear}`;
+footer.appendChild(copyright);
 
-const skills = ["JavaScript", "HTML", "CSS", "Technical writer"]
-const skillsSection = document.getElementById('skills')
-const skillsList = skillsSection.querySelector('ul')
-for (let i = 0; i < skills.length; i++){
-    let skill = document.createElement('li')
-    skill.innerHTML = skills[i]
-    skillsList.appendChild(skill)
+const skills = ["JavaScript", "HTML", "CSS", "Technical writer"];
+const skillsSection = document.getElementById("skills");
+const skillsList = skillsSection.querySelector("ul");
+for (let i = 0; i < skills.length; i++) {
+  let skill = document.createElement("li");
+  skill.innerHTML = skills[i];
+  skillsList.appendChild(skill);
 }
-
 
 //DOM selection
 const messageForm = document.getElementById("form");
 
 //Event listener for form submission
 messageForm.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const usersName = e.target.usersName.value
-    const usersEmail = e.target.usersEmail.value
-    const usersMessage = e.target.usersMessage.value
+  e.preventDefault();
+  const usersName = e.target.usersName.value;
+  const usersEmail = e.target.usersEmail.value;
+  const usersMessage = e.target.usersMessage.value;
 
-    console.log("Name:", usersName)
-    console.log("Email:", usersEmail)
-    console.log("Message:", usersMessage)
-    const messageSection = document.getElementById("messages")
-    const messageList = messageSection.querySelector("ul")
-    const newMessage = document.createElement("li")
-    newMessage.innerHTML = `<a href="mailto:adams41191@yahoo.com">${usersName}</a>
-    <span>wrote: &nbsp${usersMessage}</span>`
+  console.log("Name:", usersName);
+  console.log("Email:", usersEmail);
+  console.log("Message:", usersMessage);
+  const messageSection = document.getElementById("messages");
+  const messageList = messageSection.querySelector("ul");
+  const newMessage = document.createElement("li");
+  newMessage.innerHTML = `<a href="mailto:adams41191@yahoo.com">${usersName}</a>
+    <span>wrote: &nbsp${usersMessage}</span>`;
 
-    const removeButton = document.createElement("button")
-    removeButton.textContent="remove"
-    removeButton.type = "button"
-    removeButton.addEventListener("click", () => {
-        const entry = removeButton.parentNode;
-        entry.remove()
-    })
-    newMessage.appendChild(removeButton)
-    messageList.appendChild(newMessage)
-    messageForm.reset()
-
-})
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "remove";
+  removeButton.type = "button";
+  removeButton.addEventListener("click", () => {
+    const entry = removeButton.parentNode;
+    entry.remove();
+  });
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+  messageForm.reset();
+});
 
 /*// adding GitHub links using AJAX
 
@@ -78,24 +76,19 @@ githubRequest.addEventListener('load', (event) => {
 fetchData();
 
 function fetchData() {
-    fetch('https://api.github.com/users/SashaFoxx/repos')
-    .then(res => res.json())
-    .then(data => {
-        let projectSection = document.getElementById('projects')
-        let projectList = projectSection.querySelector('ul')
-        for (let i = 0; i < data.length; i++) {
-            let project = document.createElement('li')
-            project.innerHTML += `<a href="${data[i].html_url}" target="_blank">${data[i].name}</a>`
-            projectList.appendChild(project)
-
-    }})
+  fetch("https://api.github.com/users/SashaFoxx/repos")
+    .then((res) => res.json())
+    .then((data) => {
+      let projectSection = document.getElementById("projects");
+      let projectList = projectSection.querySelector("ul");
+      for (let i = 0; i < data.length; i++) {
+        let project = document.createElement("li");
+        project.innerHTML += `<a href="${data[i].html_url}" target="_blank">${data[i].name}</a>`;
+        projectList.appendChild(project);
+      }
+    })
 
     .catch((error) => {
-        console.error("error fetching data:", error)
-            })
-   
+      console.error("error fetching data:", error);
+    });
 }
-
-
-
-
